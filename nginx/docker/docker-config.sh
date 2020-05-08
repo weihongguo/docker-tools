2080353e9def717a28827f4388f33dae88bc5579
weihongguo <35117668@qq.com>
2020-05-08 21:51:30 +0800

    gitignore

--- nginx/docker/docker-config.sh
+++ nginx/docker/docker-config.sh
@@ -2,4 +2,4 @@
 
 image=nginx
 container=nginx
-vars="-p 80:80 -v ${PWD}/../www:/www -v ${PWD}/../etc/nginx.conf:/etc/nginx/nginx.conf -v ${PWD}/../etc/conf.d:/etc/nginx/conf.d -v ${PWD}/../logs:/var/log/nginx"
+vars="-p 80:80 -p 81:81 -v ${PWD}/../www:/www -v ${PWD}/../etc/nginx.conf:/etc/nginx/nginx.conf -v ${PWD}/../etc/conf.d:/etc/nginx/conf.d -v ${PWD}/../logs:/var/log/nginx"
